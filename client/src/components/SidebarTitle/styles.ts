17b824741bcff08dde0517dc67c2ca3e9e8cb422
Axel Thiel <axth2220@gmail.com>
2021-09-14 16:25:07 +0200

    add styles to sidebar

--- client/src/components/SidebarTitle/styles.ts
+++ client/src/components/SidebarTitle/styles.ts
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+export const Container = styled.div`
+  position: relative;
+  border-radius: 10px;
+  width: 255px;
+  height: 137px;
+  background: url("assets/suggestions/desktop/background-header.png");
+`;
+
+export const TitleContainer = styled.div`
+  position: absolute;
+  bottom: 24px;
+  left: 24px;
+`;
+
+export const Title = styled.h2`
+  font-size: 20px;
+  font-weight: 700;
+  color: #ffffff;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 15px;
+  opacity: 0.75;
+  color: #ffffff;
+`;
