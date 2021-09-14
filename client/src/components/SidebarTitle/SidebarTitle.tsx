e8211007faa68db9f9594160178a1b2bb3874b5f
Axel Thiel <axth2220@gmail.com>
2021-09-14 16:21:15 +0200

    remove boilerplate code

--- client/src/components/SidebarTitle/SidebarTitle.tsx
+++ client/src/components/SidebarTitle/SidebarTitle.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Container, TitleContainer, Title, Subtitle } from './styles'
 
 export const SidebarTitle = () => {
     return (
-        <div>
-            Sidebar title
-        </div>
+        <Container>
+            <TitleContainer>
+                <Title>Frontend Mentor</Title>
+                <Subtitle>Feedback Board</Subtitle>
+            </TitleContainer>
+            
+        </Container>
     )
 }
