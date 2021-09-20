f15739757ab3b11a61c741e34215d8ddb954f14f
Axel Thiel <axth2220@gmail.com>
2021-09-20 15:16:26 +0200

    add working filter

--- client/src/components/FilterButton/FilterButton.tsx
+++ client/src/components/FilterButton/FilterButton.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import * as S from './styles'
+import {filters} from '../SuggestionFilter/SuggestionsFilter';
 
 type FilterbuttonProps = {
-    filterText: string
+    filterText: typeof filters[number]
+    selected: boolean,
+    callback: (filterValue: typeof filters[number]) => void
 }
+export const FilterButton = ({filterText, selected, callback}: FilterbuttonProps) => {
 
-export const FilterButton = ({filterText}: FilterbuttonProps) => filterText === 'All' ? <S.FilterButtonSelected>{filterText}</S.FilterButtonSelected> : <S.FilterButton>{filterText}</S.FilterButton>     
+    return selected ? <S.FilterButtonSelected>{filterText}</S.FilterButtonSelected> : <S.FilterButton onClick={() => callback(filterText)}>{filterText}</S.FilterButton>
+}     
 
--- client/src/components/SuggestionFilter/SuggestionsFilter.tsx
+++ client/src/components/SuggestionFilter/SuggestionsFilter.tsx
@@ -1,15 +1,22 @@
-import React, {useState} from 'react'
+import React, {useContext, useCallback} from 'react'
 import { FilterContainer } from './styles'
 import { FilterButton } from '../FilterButton/FilterButton'
+import {FilterContext} from '../../context/FilterProvider';
+
+export const filters = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"] as const
 
 
 export const SuggestionsFilter = () => {
-    const [filterData, setFilterData] = useState<string[]>(["All", "UI", "UX", "Enhancement", "Bug", "Feature"])
+    const {filterValue, setFilterValue} = useContext(FilterContext)
+    
+    const selectFilter = useCallback((value) => {
+        setFilterValue(value);
+    }, [setFilterValue])
 
     return (
         <FilterContainer>
-            {filterData.map(filter => {
-                return <FilterButton filterText={filter}/>
+            {filters.map(filter => {
+                return <FilterButton filterText={filter} selected={filter === filterValue} callback={selectFilter}/>
             })}            
         </FilterContainer>
     )
